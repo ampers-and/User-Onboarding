042db5b6aadbfadc70cf5418b0848476c73469d9
ampersand <ampersand@nhs-MacBook-Pro.local>
2019-08-22 15:28:39 -0700

    mvp + dropdown

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,50 +3,84 @@ import axios from "axios";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 
-const Form = () => {
+const UserForm = ({ values, status}) => {
 
     const [user, setUser] = useState([]);
 
+    useEffect( () => {
+        if (status) {
+            setUser([...user, status]);
+        }
+    }, [status]);
+
     return (
-
-        <div className='form'>
-            <form onSubmit={event => handleSubmit(event)}>
-
-                <label>
-                    Name:
-                    <input
-                        type="text"
-                        name="name"
-                        onChange={event => handleChange(event)} />
-                </label>
-
-                <label>
-                    Email:
-                    <input
-                        type='text'
-                        name='email'
-                        onChange={event => handleChange(event)} />
-                </label>
-
-                <label>
-                    Password:
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={event => handleChange(event)}/>
-                </label>
-
-                <label>
+        <div className='user-form'>
+            <h1>Sign Up</h1>
+            <Form>
+                <Field type="text" name="name" placeholder="Name" />
+                <Field type="text" name="email" placeholder="Email" />
+                <Field type="password" name="password" placeholder="Password" />
+                <Field component="select" className="role-select" name="role">
+                    <option>Please Choose a Role</option>
+                    <option value="Gryffindor">Gryffindor</option>
+                    <option value="Slytherin">Slytherin</option>
+                    <option value="Ravenclaw">Ravenclaw</option>
+                    <option value="Hufflepuff">Hufflepuff</option>
+                </Field>
+                <label className="checkbox-container">
                     I accept the Terms of Service
-                    <input 
-                        type='checkbox'
-                        name='termsOfService'
-                        onChange={event => handleChange(event)} required/>
+                    <Field
+                        type="checkbox"
+                        name="termsOfService"
+                        checked={values.termsOfService}
+                        required/>
+                    <span className="checkmark" />
                 </label>
-                
 
                 <button>Submit!</button>
-            </form>
+            </Form>
+
+            {user.map(u => (
+                <ul className={u.role} key={u.id}>
+                <li>Name: {u.name} </li>
+                <li>Email: {u.email} </li>
+                <li>Role: {u.role} </li>
+                </ul>
+            ))}
+
         </div>
     )
 }
+
+  const FormikUserForm = withFormik({
+
+    mapPropsToValues({ name, email, password, role, termsOfService }) {
+      return {
+        name: name || "",
+        email: email || "",
+        password: password || "",
+        role: role || "",
+        termsOfService: termsOfService || false
+      };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Please Enter A Name"),
+        email: Yup.string().required("Please Enter An Email Address"),
+        password: Yup.string().min(6).required("Password Must be at least 6 Characters")
+
+      }),
+  
+    handleSubmit(values, { setStatus }) {
+
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        setStatus(res.data);
+      })
+      .catch(err => console.log(err.response));
+  }
+
+  })(UserForm);
+  
+  export default FormikUserForm;
